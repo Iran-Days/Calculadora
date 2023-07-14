7bbcb484a1aa2fa6b9f983e471118e8ced80b3d5
Iran-Days <125934570+Iran-Days@users.noreply.github.com>
2023-07-13 22:50:29 -0300

    Calculadora

---
+++ calculo.py
@@ -0,0 +1,12 @@
+'''
+Um programa que receba dois números inteiros e mostra o resultado da soma
+da multiplicção, da divisão e subtração de um número pelo outro. 
+'''
+numero_1 = int(input("Informe um Número:"))
+numero_2 =  int(input("Informe outro Número:"))
+resultado_soma = numero_1 + numero_2
+resultado_multiplicação = numero_1 * numero_2
+resultado_subtração = numero_1 - numero_2
+resultado_divisão = numero_1 / numero_2
+print("A Soma Foi:", resultado_soma, "A Multiplicação Foi:", resultado_multiplicação,
+       "A Subtração Foi:", resultado_subtração, "A Divisão Foi:", resultado_divisão)
